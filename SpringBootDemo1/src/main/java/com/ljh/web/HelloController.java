65f1eaa7db5703705eb94049b064b21c3e409bd9
Lance8931 <1838133252@qq.com>
2018-03-22 15:26:02 +0800

    测试使用springboot自带的logback日志管理输出文件的功能

--- SpringBootDemo1/src/main/java/com/ljh/web/HelloController.java
+++ SpringBootDemo1/src/main/java/com/ljh/web/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	public String index() {
-		return "Hello World.";
+		return "Hello World1.";
 	}
 }
--- SpringBootDemo1/src/test/java/com/ljh/SpringBootDemo1ApplicationTests.java
+++ SpringBootDemo1/src/test/java/com/ljh/SpringBootDemo1ApplicationTests.java
@@ -33,7 +33,7 @@ public class SpringBootDemo1ApplicationTests {
 
 	@Test
 	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World."))); 
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World1."))); 
 	}
 }
